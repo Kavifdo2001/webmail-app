665cefd1fdff9a45b474c7a9e878496fd44ac613
kavindu Nimalsha <125443904+Kavifdo2001@users.noreply.github.com>
2026-07-06 13:48:52 +0530

    update

--- resources/views/mailbox/sent.blade.php
+++ resources/views/mailbox/sent.blade.php
@@ -47,7 +47,16 @@
                             {{ $msg->body }}
                         </td>
                         <td scope="row" class="px-6 py-4 font-medium text-heading whitespace-nowrap">
-                            View 
+                            <a href="{{ route('mail.view',$msg->id) }}">
+                                <svg xmlns="http://w3.org" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                    stroke="currentColor"
+                                    class="w-6 h-6 text-gray-600 hover:text-blue-600 transition-colors">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                </svg>
+                            </a>
                         </td>
 
 
